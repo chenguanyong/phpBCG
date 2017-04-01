50a45b88920855139cb00962f59a46d152d3b593
chen <993238441@qq.com>
2017-04-01 08:25:12 +0800

    update
    2017/4/1

--- application/empty/mode/Index.php
+++ application/empty/mode/Index.php
@@ -3,10 +3,34 @@ namespace app\common\mode;
 use \think\Model;
 class Index extends Model
 {
+    protected $table = '';//操作的表名
+    protected $pk = '';//主键名称
+    //初始化
     public function initialize(){
-    
         parent::initialize();
     }
+    //添加数据
+    public function add(){
+        
+    } 
+    //删除数据
+    public function delete(){
+        
+    }
+    //更新数据
+    public function update(){
+        
+    }
+    //查询数据
+    public function select(){
+        
+    }
+    //获取指定条件的数据数量
+    public function getCountByWhere($where){
+        
+    }
+    //
+    
 }
 
 ?>
